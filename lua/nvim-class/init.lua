d366727130ca1b3c88f4f7d90a95c6ef26b00fde
DengShilin <dengsl.dev@gmail.com>
2024-09-14 17:57:57 +0800

    first

--- lua/nvim-class/init.lua
+++ lua/nvim-class/init.lua
@@ -1,1 +1,10 @@
-print(1)
+local get_current_file_path = function()
+  local current_buffer_id = vim.api.nvim_get_current_buf()
+  local current_file_path = vim.api.nvim_buf_get_name(current_buffer_id)
+  return current_file_path
+end
+
+-- java -jar fernflower.jar -dgs=1 c:\Temp\binary\library.jar c:\Temp\binary\Boot.class c:\Temp\source\
+
+
+print(get_current_file_path())

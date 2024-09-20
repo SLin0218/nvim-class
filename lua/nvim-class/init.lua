c2bb0c2f1f92c197258bb0ae77c8b9b47e771a94
DengShilin <dengsl.dev@gmail.com>
2024-09-20 15:19:20 +0800

    feat: add fernflower.jar

--- lua/nvim-class/init.lua
+++ lua/nvim-class/init.lua
@@ -6,5 +6,17 @@ end
 
 -- java -jar fernflower.jar -dgs=1 c:\Temp\binary\library.jar c:\Temp\binary\Boot.class c:\Temp\source\
 
-
 print(get_current_file_path())
+
+local function decompile_class_file(file_path)
+  return '12345'
+end
+
+vim.api.nvim_create_autocmd('BufReadPre', {
+  pattern = '*.class',
+  callback = function()
+    local file_path = vim.fn.expand('%:p') -- 获取 .class 文件的路径
+    local decompiled_content = decompile_class_file(file_path)
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, decompiled_content)
+  end,
+})
